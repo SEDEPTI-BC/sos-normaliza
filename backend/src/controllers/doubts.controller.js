90cfeefd0ea25d0351593a2db768642fa2f5234c
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-03 22:33:29 -0300

    feat: update controllers

--- backend/src/controllers/doubts.controller.js
+++ backend/src/controllers/doubts.controller.js
@@ -1,7 +1,28 @@
-class Doubt {
+const DoubtsRepository = require('../repositories/doubts.repository');
+
+class DoubtController {
   async index (req, res, next) {
-    res.send('Dúvidas');
+    res.json({ status: 'Endpoint available', endpoint: '/duvidas/' });
+  }
+
+  async create (req, res, next) {
+    const { title } = req.body;
+
+    try {
+      await DoubtsRepository.create({ title });
+
+      return res
+        .status(201)
+        .json({ message: 'Doubt has been created' });
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ 
+          message: 'An unexpected error during creating doubt',
+          log: '`${e}`'
+        });
+    }
   }
 }
 
-module.exports = new Doubt;
+module.exports = new DoubtController;
--- backend/src/controllers/time.controller.js
+++ backend/src/controllers/time.controller.js
@@ -1,7 +1,28 @@
-class Time {
+const TimeRepository = require('../repositories/time.repository');
+
+class TimeController {
   async index (req, res, next) {
-    res.send('Horários');
+    res.json({ status: 'Endpoint available', endpoint: '/horarios/' });
+  }
+
+  async create (req, res, next) {
+    const { hour } = request.body;
+
+    try {
+      await TimeRepository.create({ hour });
+
+      return res
+        .status(201)
+        .json({ message: 'Hour registered in database'});
+    } catch (e) {
+      return res
+        .status(400)
+        .json({
+          message: 'An unexpected error occurred during registry hour',
+          log: '`${e}`'
+        });
+    }
   }
 }
 
-module.exports = new Time;
+module.exports = new TimeController;
