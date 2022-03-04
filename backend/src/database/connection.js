627e0b83feb0716cff27925d00207454fd6a6a09
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2022-03-03 22:33:08 -0300

    feat: database status connection

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const database = require('../config/database');
 
 const connection = database
-  .authenticate();
+  .authenticate()
+  .then(() => console.log(`Connection with database established with success`))
+  .catch(e => console.error(`Error during establish connection with database. Log: ${e}`));
 
 module.exports = connection;
